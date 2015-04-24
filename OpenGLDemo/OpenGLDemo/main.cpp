9868934e48b23c48976e0e5a3e3b03dd21e4d531
colin3dmax <colin3dmax@163.com>
2015-04-24 09:24:33 +0800

    add first xcode opengl demo

---
+++ OpenGLDemo/OpenGLDemo/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by colin on 15/4/24.
+//  Copyright (c) 2015年 colin. All rights reserved.
+//
+
+#include <iostream>
+
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+
+#pragma clang  diagnostic ignored "-Wdeprecated-declarations"
+
+void init()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0f, 0.0f, 1.0, -1.0, 1.0);
+    
+}
+
+void display()
+{
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glColor3f(1.0, 1.0f, 1.0f);
+    glBegin(GL_POLYGON);
+    glVertex3f(0.25, 0.25, 0.0);
+    glVertex3f(0.75, 0.25, 0.0);
+    glVertex3f(0.75, 0.75, 0.0);
+    glVertex3f(0.25, 0.75, 0.0);
+    glEnd();
+    
+    glFlush();
+}
+
+int main(int argc, char ** argv) {
+    glutInit(&argc, argv);
+    
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    
+    return 0;
+}
